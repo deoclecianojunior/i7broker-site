48b02b628ce95a801641e9fcd9a9f7bd230d4c31
Deocleciano Junior <dperesjr@gmail.com>
2021-10-09 23:24:01 -0300

    Add ci/cd

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,9 @@ import '../public/assets/css/all.min.css';
 import '../public/assets/webfonts/flaticon/flaticon.css';
 import '../public/assets/css/owl.css';
 import '../public/assets/css/jquery.fancybox.min.css';
-import '../public/assets/css/template.css';
-import '../public/assets/css/style.css';
-import '../public/assets/css/colors/color.css';
+import '../public/assets/css/template.min.css';
+import '../public/assets/css/style.min.css';
+import '../public/assets/css/colors/color.min.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
